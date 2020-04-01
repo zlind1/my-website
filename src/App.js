a566713330d2751ea4e8be5f94775cf8b2cba98d
Zach <zlind@ucsc.edu>
2020-04-01 16:03:19 -0700

    removed fontawesome dependency

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faKey} from '@fortawesome/free-solid-svg-icons';
+
+import TopBar from './TopBar.js';
 
 function App() {
   return (
-    <Container className='bg-primary'>
-      <h1>Zach Lind</h1>
-      <p>Software Engineer, Full Stack Web Developer, Game Designer</p>
-      <FontAwesomeIcon icon={faKey} />
-    </Container>
+    <>
+    <TopBar />
+      <Container className='bg-primary'>
+        <h1>Zach Lind</h1>
+        <p>Software Engineer, Full Stack Web Developer, Game Designer</p>
+        
+      </Container>
+    </>
   );
 }
 
