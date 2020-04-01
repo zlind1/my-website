469eda4a98237acbb47ef45f63cf10cd7e07e60d
Zach <zlind@ucsc.edu>
2020-04-01 13:39:33 -0700

    added react-bootstrap and react-fontawesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Container} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faKey} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <div>
-      <h1>btch</h1>
-    </div>
+    <Container className='bg-primary'>
+      <h1>Zach Lind</h1>
+      <p>Software Engineer, Full Stack Web Developer, Game Designer</p>
+      <FontAwesomeIcon icon={faKey} />
+    </Container>
   );
 }
 
