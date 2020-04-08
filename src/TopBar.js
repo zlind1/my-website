3fa1317df1c336d96049787b8d06da23105d0fa5
Zach <zlind@ucsc.edu>
2020-04-08 01:59:21 -0700

    fixed col spacing in topbar

--- src/TopBar.js
+++ src/TopBar.js
@@ -5,16 +5,16 @@ import {Link} from 'react-router-dom';
 function TopBar() {
   return (
     <Row className='bg-dark'>
-    	  <Link to='/' as={Col}>
+    	  <Link to='/' as={Col} xs={3}>
     	    Home
     	  </Link>
-    	  <Link to='/resume' as={Col}>
+    	  <Link to='/resume' as={Col} xs={3}>
     	    Resume
     	  </Link>
-    	  <Link to='/portfolio' as={Col}>
+    	  <Link to='/portfolio' as={Col} xs={3}>
     	    Portfolio
     	  </Link>
-    	  <Link to='/contact' as={Col}>
+    	  <Link to='/contact' as={Col} xs={3}>
     	    Contact
     	  </Link>
     </Row>
