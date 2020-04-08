f082288d33157952a143430d0480996d87e39cb7
Zach <zlind@ucsc.edu>
2020-04-08 01:46:27 -0700

    added routing and links in topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Container} from 'react-bootstrap';
+import {Route} from 'react-router-dom';
 
 import TopBar from './TopBar.js';
 
@@ -7,6 +8,18 @@ function App() {
   return (
     <Container fluid>
       <TopBar />
+      <Route exact path='/'>
+        home
+      </Route>
+      <Route exact path='/resume'>
+        resume
+      </Route>
+      <Route exact path='/portfolio'>
+        portfolio
+      </Route>
+      <Route exact path='/contact'>
+        contact
+      </Route>
     </Container>
   );
 }
--- src/TopBar.js
+++ src/TopBar.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {Row} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 function TopBar() {
   return (
     <Row className='bg-primary'>
-    	  nav bar
+    	  <Link to='/'>
+    	    Home
+    	  </Link>
+    	  <Link to='/resume'>
+    	    Resume
+    	  </Link>
+    	  <Link to='/portfolio'>
+    	    Portfolio
+    	  </Link>
+    	  <Link to='/contact'>
+    	    Contact
+    	  </Link>
     </Row>
   );
 }
