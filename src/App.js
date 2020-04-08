a1c1964711337edb59586083b86c37e687ec43f7
Zach <zlind@ucsc.edu>
2020-04-08 00:34:51 -0700

    removed tuner, reworking design

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import {Container} from 'react-bootstrap';
 
-import TopBar from './TopBar.js';
+import Tuner from './Tuner.js';
 
 function App() {
   return (
     <>
-    <TopBar />
-      <Container className='bg-primary'>
-        <h1>Zach Lind</h1>
-        <p>Software Engineer, Full Stack Web Developer, Game Designer</p>
-        
-      </Container>
+      <TopBar />
     </>
   );
 }
---
+++ src/TopBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function TopBar() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default TopBar;
