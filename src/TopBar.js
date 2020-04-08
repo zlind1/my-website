ffea6d23f6f8dc5b5cf2387619ecdc781eaa1c2b
Zach <zlind@ucsc.edu>
2020-04-08 01:55:01 -0700

    changed topbar color, moved links to cols

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import {Row} from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 function TopBar() {
   return (
-    <Row className='bg-primary'>
-    	  <Link to='/'>
+    <Row className='bg-dark'>
+    	  <Link to='/' as={Col}>
     	    Home
     	  </Link>
-    	  <Link to='/resume'>
+    	  <Link to='/resume' as={Col}>
     	    Resume
     	  </Link>
-    	  <Link to='/portfolio'>
+    	  <Link to='/portfolio' as={Col}>
     	    Portfolio
     	  </Link>
-    	  <Link to='/contact'>
+    	  <Link to='/contact' as={Col}>
     	    Contact
     	  </Link>
     </Row>
