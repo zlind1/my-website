85e250b25ca8fa0aafa7f7298f6e2fdd2f88cfd7
Zach Lind <zacharywolfflind@gmail.com>
2021-07-26 12:55:36 -0700

    change form submission

--- src/Contact.js
+++ src/Contact.js
@@ -2,20 +2,38 @@ import React from 'react';
 import {Form, Button} from 'react-bootstrap';
 
 function Contact() {
-  function submitForm() {
-    alert('Thanks for reaching out, I\'ll get in touch in the next couple days.');
+  const email = React.useRef();
+  const message = React.useRef();
+  function encode(data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
+  }
+  function submitForm(e) {
+    e.preventDefault()
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': e.target.getAttribute('name'),
+        'email': email.current.value,
+        'message': message.current.value
+      })
+    }).then(() => console.log('form submitted')).catch(error => alert(error))
   }
   return <>
     <h1>Feel free to send me a message and we can chat.</h1>
-    <Form data-netlify='true' name='contact' onSubmit={submitForm}>
+    <Form data-netlify='true' name='contact' onSubmit={submitForm} method='post'>
       <input type='hidden' name='form-name' value='contact' />
       <Form.Group>
         <Form.Label>Email address</Form.Label>
-        <Form.Control placeholder='example@gmail.com' type='email' name='email' required/>
+        <Form.Control placeholder='example@gmail.com' type='email'
+          name='email' ref={email} required/>
       </Form.Group>
       <Form.Group>
         <Form.Label>Message</Form.Label>
-        <Form.Control as='textarea' placeholder='Enter your message here' name='message' required/>
+        <Form.Control as='textarea' placeholder='Enter your message here'
+          name='message' ref={message} required/>
       </Form.Group>
       <Button type='submit'>Send message</Button>
     </Form>
