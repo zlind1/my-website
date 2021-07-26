b0d271e986063736cb52c571ab8a925e2c09990c
Zach Lind <zacharywolfflind@gmail.com>
2021-07-26 12:45:38 -0700

    added netlify form for contact

--- src/Contact.js
+++ src/Contact.js
@@ -2,23 +2,20 @@ import React from 'react';
 import {Form, Button} from 'react-bootstrap';
 
 function Contact() {
-  function sendMessage(e) {
-    e.preventDefault();
-    console.log(email.current.value);
-    console.log(message.current.value);
+  function submitForm() {
+    alert('Thanks for reaching out, I\'ll get in touch in the next couple days.');
   }
-  const email = React.useRef();
-  const message = React.useRef();
   return <>
     <h1>Feel free to send me a message and we can chat.</h1>
-    <Form onSubmit={sendMessage}>
+    <Form data-netlify='true' name='contact' onSubmit={submitForm}>
+      <input type='hidden' name='form-name' value='contact' />
       <Form.Group>
         <Form.Label>Email address</Form.Label>
-        <Form.Control placeholder='example@gmail.com' type='email' ref={email} required/>
+        <Form.Control placeholder='example@gmail.com' type='email' name='email' required/>
       </Form.Group>
       <Form.Group>
         <Form.Label>Message</Form.Label>
-        <Form.Control as='textarea' placeholder='Enter your message here' ref={message} required/>
+        <Form.Control as='textarea' placeholder='Enter your message here' name='message' required/>
       </Form.Group>
       <Button type='submit'>Send message</Button>
     </Form>
